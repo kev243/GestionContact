83e820f478942c00781e9843a679820d17a219c0
nimikevin <97411659+nimikevin@users.noreply.github.com>
2022-11-08 00:28:36 -0500

    travail sur la methode get contact

--- src/Contact.java
+++ src/Contact.java
@@ -1,3 +1,42 @@
 public class Contact {
-    
+    private String lastName;
+    private String firstName;
+    private String phoneNumber;
+
+
+    public Contact (String lastName, String firstName, String phoneNumber){
+        this.lastName = lastName;
+        this.firstName= firstName;
+        this.phoneNumber=phoneNumber;
+    }
+
+    //get et le set
+
+    public String getLastName(String lastName){
+        return lastName;
+    }
+    public void setLastName(String firstName){
+        this.lastName=lastName;
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public void setFirstName(String firstName){
+        this.firstName=firstName;
+    }
+
+    public String getPhoneNumber(){
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber){
+        this.phoneNumber=phoneNumber;
+    }
+
+    @Override
+    public String toString(){
+        return  this.lastName+" "+ this.firstName+" "+ phoneNumber;
+    }
 }
--- src/PhoneBook.java
+++ src/PhoneBook.java
@@ -1,24 +1,79 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class PhoneBook {
     public static  Scanner sc =null;
     public static void main(String[] args) {
 
-       sc= new Scanner(System.in);
+        sc = new Scanner(System.in);
 
-        String userLastName = getUserInput("Entrez un nom de famille");
-        String userFirstName = getUserInput("Entrez un prénom");
-        String userPhoneNumber = getUserInput("Entrez un numéro de téléphone");
+        String userLastName = getUserInput("Entrez un nom de famille: ");
+        String userFirstName = getUserInput("Entrez un prénom: ");
+        String userPhoneNumber = getUserInput("Entrez un numéro de téléphone: ");
+
+        Contact newContact = new Contact(userLastName, userFirstName, userPhoneNumber);
+        System.out.println(newContact.toString());
 
 
-        sc.close();
+         File phoneBookFile = getOrCreatePhoneBookFile("/Users/kevin/coursJava/phonebook.txt");
+
+       
+
+
+
+            //Gestion d'erreur
+            try {
+                BufferedWriter fileWriter = new BufferedWriter(new FileWriter(phoneBookFile, true));
+                fileWriter.append(newContact.toString());
+                fileWriter.append('\n');
+                System.out.println("contact ajouté");
+                fileWriter.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            sc.close();
+        }
+
+
+        public static String getUserInput (String userRequest){
+            if (userRequest == null || sc == null) {
+                return null;
+            }
+
+            System.out.println(userRequest);
+            return sc.nextLine();
+
+        }
+
+    public static File getOrCreatePhoneBookFile(String phoneBookFilePath){
+        //localisation de notre fichier
+        File phoneBookFile = new File(phoneBookFilePath);
+
+        //test si le fichier existe ou pas
+        if (phoneBookFile.exists()){
+            //System.out.println("le fichier existe");
+            return phoneBookFile;
+
+        }
+            //System.out.println("Le fichier n'existe pas")
+
+            try{
+                phoneBookFile.createNewFile();
+                System.out.println("le fichier a été crée");
+                return phoneBookFile;
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+
+
+       return  null;
     }
 
-        //methode pour donner la main au user
-    public static  String getUserInput(String userRequest){
-
-        System.out.println(userRequest);
-        return sc.nextLine();
-
     }
-}
+
+
