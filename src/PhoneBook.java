d25ebb9b19d1629e21cb51113730ef205a3e6dea
nimikevin <97411659+nimikevin@users.noreply.github.com>
2022-11-07 21:45:19 -0500

    travail sur les classe

--- src/PhoneBook.java
+++ src/PhoneBook.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class PhoneBook {
+    public static  Scanner sc =null;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+       sc= new Scanner(System.in);
+
+        String userLastName = getUserInput("Entrez un nom de famille");
+        String userFirstName = getUserInput("Entrez un prénom");
+        String userPhoneNumber = getUserInput("Entrez un numéro de téléphone");
+
+
+        sc.close();
+    }
+
+        //methode pour donner la main au user
+    public static  String getUserInput(String userRequest){
+
+        System.out.println(userRequest);
+        return sc.nextLine();
+
     }
 }
